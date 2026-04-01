d55b0c5fc324d2722c7055bee16c5eab480b704b
unknown <arzyadwan@gmail.com>
2026-04-01 14:57:42 +0800

    aplikasi dekstop tetapi cuma frontend yg jalan

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,21 @@ export default defineConfig({
     tailwindcss(),
     react(),
   ],
+  clearScreen: false,
+  envPrefix: ['VITE_', 'TAURI_'],
+
   server: {
+    port: 5173,
+    strictPort: true,
     host: true,
     proxy: {
-      '/api': 'http://localhost:3002'
+      '/api': {
+        target: 'http://localhost:3002', // URL server backend Anda
+        changeOrigin: true,
+        secure: false,
+        // Opsional: aktifkan baris di bawah ini jika backend Anda tidak menggunakan prefix /api
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
